7b07db98c8d5b99ef93aa3e3a44f00bf5232e929
Andrew Arrow <andrew@anybeat.com>
2012-04-24 17:54:01 -0700

    pages

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,15 @@
 class WelcomeController < ApplicationController
   
   def index
+    if params[:page]
+      @page = params[:page].to_i
+    else
+      @page = 1
+    end
+
+    if @page > 1
+      render action: @page.to_s
+    end
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Timekeeping::Application.routes.draw do
 
   root :to => 'welcome#index'
+  match ':page' => 'welcome#index'
 
 end
